f45fd4f1293fe8dc3e2a58296972943ed9504927
grampaa250 <asoto9997@gmail.com>
2026-09-10 22:41:27 -0500

    Change URL loading method in Loader.lua
    Updated the URL loading mechanism for the script.

--- Loader.lua
+++ Loader.lua
@@ -2,8 +2,7 @@
 local modo = getgenv().script_mode
 
 if modo == "PVP" then
-    -- REEMPLAZA ESTA URL POR TU URL RAW DEL PASO 3
-    https://raw.githubusercontent.com/grampaa250/10k-damage/fb89ec3b4eba04a60f77db132b16e389ff01f6dc/M1Burst.lua
+    loadstring(game:httpGet("https://githubusercontent.com"))()
 else
     print("Modo no reconocido o vacio. Pon getgenv().script_mode = 'PVP'")
 end
